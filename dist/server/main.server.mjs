80750267820723ffdebcc9bf5c529740041dad14
lucas-ga <lucassilva_jlle@outlook.com>
2023-12-21 08:29:44 -0300

    correções no build

--- dist/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-FMJQKJ4K.mjs";import"./chunk-64XZ7MH2.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-CRA5CMT7.mjs";import"./chunk-OAZVD7OG.mjs";import"./chunk-FME56UVT.mjs";export{a as default};
