adc6e88630eb22ae42604f9c6d947a584421e074
lucas-ga <lucassilva_jlle@outlook.com>
2023-12-21 16:53:35 -0300

    atualizando usuários estáricos

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -12,7 +12,7 @@ export class UserListComponent implements OnInit{
 
   users = [
     { name: 'Lucas', birthDate: '15/08/1999', login: 'lucas.silva', password: 'leleh'},
-    { name: 'Letícia', birthDate: '27/04/2002', login: 'leticia.menega', password: 'luquinhas'},
+    { name: 'Letícia', birthDate: '27/04/2002', login: 'leticia.menegazzi', password: 'luquinhas'},
     { name: 'zequinha', birthDate: '01/01/1864', login: 'ze.quinha', password: 'zeze'}
   ]
 }
