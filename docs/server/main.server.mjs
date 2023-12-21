d528748729e62a1d1d0a3cbe409289bfdbc2986b
lucas-ga <lucassilva_jlle@outlook.com>
2023-12-21 09:50:37 -0300

    alterações

--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-CRA5CMT7.mjs";import"./chunk-OAZVD7OG.mjs";import"./chunk-FME56UVT.mjs";export{a as default};
--- docs/server/render-utils.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{Sa as o,Ta as r,Ua as n,xa as e}from"./chunk-OAZVD7OG.mjs";import"./chunk-FME56UVT.mjs";export{n as renderApplication,r as renderModule,e as \u0275Console,o as \u0275SERVER_CONTEXT};
